a2a7a858544120560e83730ae1fed509aa2600f6
Your Name <you@example.com>
2022-12-12 14:14:51 -0300

    update

---
+++ aula_knex/template-knex/build/types.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CATEGORIA = void 0;
+var CATEGORIA;
+(function (CATEGORIA) {
+    CATEGORIA["ACESSORIOS"] = "acess\u00F3rios";
+    CATEGORIA["CALCADOS"] = "cal\u00E7ados";
+    CATEGORIA["ROUPAS"] = "roupas";
+})(CATEGORIA = exports.CATEGORIA || (exports.CATEGORIA = {}));
+//# sourceMappingURL=types.js.map
